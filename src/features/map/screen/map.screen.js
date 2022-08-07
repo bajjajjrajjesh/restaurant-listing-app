b4b7a84c777135a8a232c6010800a9755e69dc79
Rajesh Bajaj <rajeshbajaj@live.in>
2022-08-07 10:06:15 +0530

    maps implementation

--- src/features/map/screen/map.screen.js
+++ src/features/map/screen/map.screen.js
@@ -1,11 +1,37 @@
 import React from "react";
-import { Text } from "../../../components/typography/text.component";
 import { SafeArea } from "../../../utils/safe-area.component";
+import MapView from "react-native-maps";
+import { StyleSheet, View, Dimensions } from "react-native";
+import { PROVIDER_GOOGLE } from "react-native-maps";
 
 export const MapScreen = () => {
   return (
     <SafeArea>
-      <Text variant="body">Maps</Text>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 22.6241504,
+            longitude: 88.4306945,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
     </SafeArea>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
